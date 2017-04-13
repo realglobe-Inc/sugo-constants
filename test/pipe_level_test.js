5f42abadd0cf4f723ce00722e26977e187b146b4
Taka Okunishi <okunishinish@gmail.com>
2017-04-13 15:42:23 +0900

    Add level

---
+++ test/pipe_level_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for pipeLevel.
+ * Runs with mocha.
+ */
+'use strict'
+
+const pipeLevel = require('../lib/pipe_level.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('pipe-level', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Pipe level', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
